789a82d60a52f2b16934c24e1386a6dc72e9b2d8
David Lemberger <dlmutiny@users.noreply.github.com>
2025-02-08 15:18:41 -0800

    Update windmap.js

--- scripts/windmap.js
+++ scripts/windmap.js
@@ -1,12 +1,20 @@
 const WINDY_API_KEY = "y1esYKpYs4uYBBhxZtIH3nJ90gvCU7JH";
 
-windyInit({
-    key: WINDY_API_KEY,
-    lat: 36.95,
-    lon: -121.96,
-    zoom: 10,
-    overlay: "wind",
-}, windyAPI => {
-    const { map } = windyAPI;
-    console.log("Windy Map initialized successfully!");
-});
+async function initializeWindyMap() {
+    if (typeof windyInit === "undefined") {
+        console.error("Windy API is not loaded correctly.");
+        return;
+    }
+
+    windyInit({
+        key: WINDY_API_KEY,
+        lat: 36.9514,
+        lon: -121.9664,
+        zoom: 7,
+        overlay: 'wind',
+    }, windyAPI => {
+        console.log("Windy Map Initialized!");
+    });
+}
+
+document.addEventListener("DOMContentLoaded", initializeWindyMap);
